f3c5dbfa79b943436a63b12c35026ad303207231
sonictrain <nicola.brucoli92@gmail.com>
2023-02-05 19:59:07 +0000

    addPoints() works now with only one function which is now able to identify the element that dispatch the event and its parent, to find how many points must be added and where.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 let homePt = 0
 let guestPt = 0
+let parent = ""
 
-document.getElementById("guest-pt").textContent = GuestPt
+document.getElementById("guest-pt").textContent = guestPt
 
-function add1() {
-    console.log("Home +1 clicked")
-    homePt += 1
-    document.getElementById("home-pt").textContent = homePt
+function addPoints() {
+    var class_name = event.target.className
+    console.log("The class is: " + class_name)
+
+    var parent_class = event.target.parentElement.className
+    console.log("The parent is: " + parent_class)
+
+    if (parent_class == "home-button-container") {
+        homePt += Number(class_name)
+        document.getElementById("home-pt").textContent = homePt
+    } else {
+        guestPt += Number(class_name)
+        document.getElementById("guest-pt").textContent = guestPt
+    }
 }
 
-function add2() {
-    console.log("Home +2 clicked")
-    homePt += 2
-    document.getElementById("home-pt").textContent = homePt
+function newSet() {
+    document.getElementById("home-pt").textContent = 0
+    document.getElementById("guest-pt").textContent = 0
 }
 
-function add3() {
-    console.log("Home +3 clicked")
-    homePt += 3
-    document.getElementById("home-pt").textContent = homePt
+function newMatch() {
+    document.getElementById("home-pt").textContent = 0
+    document.getElementById("guest-pt").textContent = 0
 }
-
